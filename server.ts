7c0f88f56fed1575225789ea8fcc237dddb2e126
laminediakite1 <laminediakiteg2001@gamil.com>
2025-04-07 14:32:05 +0200

    fit: correction cors 2

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://backendsentiment-production.up.railway.app',
+    origin: 'https://sentiment-analysis-app-phi.vercel.app',
 }));
 app.use(express.json());
 
