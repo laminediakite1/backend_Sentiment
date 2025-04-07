9d7108b65aace9cd70cdac63fab1f1d02f922873
laminediakite1 <laminediakiteg2001@gamil.com>
2025-04-07 13:46:21 +0200

    fit:+ cors

--- server.ts
+++ server.ts
@@ -5,7 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 
 app.post('/api/analyze', async (req, res) => {
